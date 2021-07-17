cdde9c8bfcc57c3355abf9ccfd0949e1dbd06391
mateusmss <mateusaldanha@usp.br>
2021-07-17 19:27:45 -0300

    gets na Sala

--- src/Gerencia/reuniao/Sala.java
+++ src/Gerencia/reuniao/Sala.java
@@ -2,5 +2,27 @@ package Gerencia.reuniao;
 
 public class Sala
 {
+    private String nome;
+    private String local;
+    private int capacidade;
+    private String observacoes;
 
+    public Sala(){
+        this.nome = nome;
+        this.local = local;
+        this.capacidade = capacidade;
+        this.observacoes = observacoes;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public String getLocal(){
+        return this.nome;
+    }
+    public int getCapacidade(){
+        return this.capacidade;
+    }
+    public String getObservacoes(){
+        return this.nome;
+    }
 }
