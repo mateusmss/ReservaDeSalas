746c346269a01bf7704b8b331060e9a8a5cc54ed
galdutro13 <gregassagraf@usp.br>
2021-07-17 19:51:08 -0300

    Reserva has changed

--- src/Gerencia/reuniao/Reserva.java
+++ src/Gerencia/reuniao/Reserva.java
@@ -1,6 +1,33 @@
 package Gerencia.reuniao;
 
+import java.util.Collection;
+
 public class Reserva
 {
+
+    //Parametros-----------------------------------------------
+    Collection<String> participantes;
+    Sala sala;
+
+    //Construtor-----------------------------------------------
+    public Reserva(Sala sala, Collection<String> participantes)
+    {
+        this.sala = sala;
+        this.participantes = participantes;
+    }
+
+//---------------------------------------------------------------
+    public void setSala(Sala sala){ this.sala = sala; }
+
+//---------------------------------------------------------------
+    public void setParticipantes(Collection<String> participantes)
+    { this.participantes = participantes; }
+
+//---------------------------------------------------------------
+    public Sala getSala(){ return sala; }
+
+//---------------------------------------------------------------
+    public Collection<String> getParticipantes(){ return participantes; }
+
 
 }
